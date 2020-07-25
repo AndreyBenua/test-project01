e1c3c7ef67f052da3be215c1046cb0015cbb4134
Andrey Benua <andrbenua@gmail.com>
2020-07-25 14:06:18 +0300

    Delete questions

--- testy_script.js
+++ testy_script.js
@@ -1,12 +1,3 @@
 'use strict';
 let metallica = ['het','ham','truch'];
 console.log(metallica);
-
-let a = prompt('Брать ли Ульриха?', '');
-
-if(a == 'да'){
-    metallica.push('ulr');
-    console.log(metallica);
-}else{
-    console.log(metallica);
-}
